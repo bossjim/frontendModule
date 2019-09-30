e82ed2f9d5b82f728eccb63efe14ad8b54f708ab
bossjim <17521335991@163.com>
2019-09-30 18:26:36 +0800

    Deer前端框架0930

--- src/views/common/PageView.vue
+++ src/views/common/PageView.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>asdf</div>
+  <page-layout :title="title">
+    <keep-alive v-if="multipage">
+      <router-view ref="page"/>
+    </keep-alive>
+    <router-view ref="page" v-else/>
+  </page-layout>
 </template>
 
 <script>
+import PageLayout from './PageLayout'
 export default {
-  name: 'PageView'
+  name: 'PageView',
+  components: {PageLayout},
+  data () {
+    return {
+      title: ''
+    }
+  },
+  computed: {
+    multipage () {
+      return this.$store.state.setting.multipage
+    }
+  },
+  mounted () {
+    this.getPageHeaderInfo()
+  },
+  updated () {
+    this.getPageHeaderInfo()
+  },
+  methods: {
+    getPageHeaderInfo () {
+      this.title = this.$route.name
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+  .extraImg{
+    margin-top: -60px;
+    text-align: center;
+    width: 195px;
+    img{
+      width: 100%;
+    }
+  }
 </style>
